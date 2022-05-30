ab9e826512da52195631782fbf053f3a4137b0f0
timmy_3000 <66499629+timmy3000@users.noreply.github.com>
2022-05-30 16:53:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import random
 
 possible_actions = ['R', 'P', 'S']
-player_score = 0
-cpu_score = 0
+
+titles = {'R': "Rock", 'P': "Paper", 'S': "Scissors"}
 
 while True:
 
@@ -12,72 +12,43 @@ while True:
     # randomizing the cpu's choice
     cpu_action = random.choice(possible_actions)
 
-    print(f"\nPlayer ({player_action}) : CPU ({cpu_action})\n")
 
     if(not(player_action in possible_actions)):
         print("Invalid entry, Please try again.\n")
         continue
 
+        
+    print(f"\nPlayer ({titles[player_action]}) : CPU ({titles[cpu_action]})\n")
+
 
     if(player_action == cpu_action):
-        player_score += 1
-        cpu_score += 1
-
         print(f"Both players selected {player_action}. It's a tie!\n")
-        print(f"You: {player_score}")
-        print(f"CPU: {cpu_score}\n")
 
     elif(player_action == "R"):
         if(cpu_action == "S"):
-            player_score += 1
-
             print("Rock beats Scissors! You win!\n")
-            print(f"You: {player_score}")
-            print(f"CPU: {cpu_score}\n")
-
+            break
         else:
-            cpu_score += 1
-
             print("Paper beats rock! You lose.\n")
-            print(f"You: {player_score}")
-            print(f"CPU: {cpu_score}\n")
+            break
 
     elif(player_action == "P"):
         if(cpu_action == "R"):
-            player_score += 1
-
             print("Paper beats rock! You win!\n")
-            print(f"You: {player_score}")
-            print(f"CPU: {cpu_score}\n")
-
+            break
         else:
-            cpu_score += 1
-
             print("Scissors beats paper! You lose.\n")
-            print(f"You: {player_score}")
-            print(f"CPU: {cpu_score}\n")
+            break
 
     elif(player_action == "S"):
-        if cpu_action == "P":
-            player_score += 1
-
+        if(cpu_action == "P"):
             print("Scissors beats paper! You win!\n")
-            print(f"You: {player_score}")
-            print(f"CPU: {cpu_score}\n")
-
+            break
         else:
-            cpu_score += 1
-
             print("Rock beats scissors! You lose.\n")
-            print(f"You: {player_score}")
-            print(f"CPU: {cpu_score}\n")
+            break
 
     else:
         print("Invalid entry.\n")
 
-
-    play_again = input("Play again? (y/n): ")
-
-    if play_again.lower() != "y":
-        break
         
